0de4d0b0c8da92b269d441f13ec9aefa2f36d947
DavidNitian <d12mondal@gmail.com>
2024-12-11 19:06:42 +0530

    person

---
+++ react1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Person from './person'
+
+const App = () => {
+  return (
+    <div>
+           <Person name="David Mondal" age={25} />
+           <Person name="Mr Mondal" age={22} />
+    </div>
+  )
+}
+
+export default App
---
+++ react1/src/Person.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Person Component
+const Person = ({ name, age }) => {
+  return (
+    <p>
+      Name: {name}, Age: {age}
+    </p>
+  );
+};
+
+export default Person;
