aa7b63bffe95e5c24a6b84da6cd4a9ab5432f258
DavidNitian <d12mondal@gmail.com>
2024-12-11 19:20:18 +0530

    Button,Header,List

--- react1/src/App.jsx
+++ react1/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Person from './person'
+import Button from './Button'
+import Header from './Header';
+import List from './List';
+
 
 const App = () => {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+  const fruits = ['Apple', 'Banana', 'Cherry'];
   return (
     <div>
+          <Header title="Wellcome to David Mondal Page .this is my header."/>
            <Person name="David Mondal" age={25} />
            <Person name="Mr Mondal" age={22} />
+           <Button text="Click Me" onClick={handleClick} />
+           <List items={fruits} />
     </div>
   )
 }
